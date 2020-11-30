e337bc22550830a77b3c73a4013c15a78df50395
dongrui.wang <dongrui.wang@mapabc.com>
2020-11-30 14:49:08 +0800

    对接红绿灯状态数据，支持闪烁功能；

--- examples/js/FacilityMgr.js
+++ examples/js/FacilityMgr.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ray
  * @Date: 2020-11-26 14:31:16
- * @LastEditTime: 2020-11-27 11:31:21
+ * @LastEditTime: 2020-11-30 14:42:48
  * @LastEditors: Please set LastEditors
  * @Description: 交通设施管理器
  * @FilePath: \edit-online\examples\js\FacilityMgr.js
@@ -13,7 +13,7 @@ function FacilityMgr(map, options) {
 }
 
 FacilityMgr.prototype.init = function () {
-    this.lights = [], this.timers = [], this.models = [];
+    this.lights = new Map(), this.timers = new Map(), this.models = [];
     var modelPath = this.options.modelPath, indexFile = this.options.indexFile;
     Cesium.Resource.fetchJson({
         url: `${modelPath}/${indexFile}`,
@@ -29,39 +29,67 @@ FacilityMgr.prototype.init = function () {
             }
             if (r.model.indexOf('HLD') >= 0) {
                 var model = new TMEarth.TrafficLight(map3d, options);
-                this.lights.push(model);
+                this.lights.set(r.id, model);
             } else if (r.model.indexOf('DJS') >= 0) {
                 var model = new TMEarth.TrafficTimer(map3d, options);
-                this.timers.push(model);
+                this.timers.set(r.id, model);
             } else {
                 this.model = new TMEarth.GltfPrimitive(map3d, options);
                 this.models.push(model);
             }
         });
-        // 更新信号灯状态
-        setInterval(() => {
-            var time = parseInt(Date.now() / 1000);
-            var light = time % 3;
-            time %= 1000;
-            this.lights.forEach(i => {
-                i.setValue(light);
-            });
-            this.timers.forEach(i => {
-                i.setValue(time);
-            });
-        }, 1000);
+        // // 更新信号灯状态
+        // setInterval(() => {
+        //     var time = parseInt(Date.now() / 1000);
+        //     var light = time % 3;
+        //     time %= 1000;
+        //     this.lights.forEach(i => {
+        //         i.setValue(light);
+        //     });
+        //     this.timers.forEach(i => {
+        //         i.setValue(time);
+        //     });
+        // }, 1000);
+    });
+}
+
+// 更新
+FacilityMgr.prototype.update = function (lampStatus) {
+    if (!lampStatus || !lampStatus.crossLamp) return;
+    lampStatus.crossLamp.forEach(lamp => {
+        // 更新红绿灯
+        var light = this.lights.get(lamp.groupId);
+        if (light && lamp.lampState != undefined) {
+            var value = [0, 0, 0], i = 0;
+            if ((i = ["R", "Y", "G"].indexOf(lamp.lampState)) >= 0) {
+                value[i] = 1;// 亮
+            } else if ((i = ["RF", "YF", "GF"].indexOf(lamp.lampState)) >= 0) {
+                value[i] = 2;// 闪
+            } else if (lamp.lampState == "RY") {
+                value = [1, 1, 0];// 红黄亮
+            }
+            light.setValue(value);
+        }
+        // 更新计时器
+        var timer = this.timers.get(lamp.groupId);
+        if (timer && lamp.remainTime) {
+            timer.setValue(remainTime);
+        }
     });
 }
 
 // 清除
 FacilityMgr.prototype.remove = function () {
-    if (this.lights) {
-        this.lights.forEach(i => i.remove());
-    }
-    if (this.timers) {
-        this.timers.forEach(i => i.remove());
-    }
+    this.lights.forEach((val, key) => {
+        val.remove();
+    });
+    this.lights.clear();
+    this.timers.forEach((val, key) => {
+        val.remove();
+    });
+    this.timers.clear();
     if (this.models) {
         this.models.forEach(i => i.remove());
     }
+    this.models.length = 0;
 }
